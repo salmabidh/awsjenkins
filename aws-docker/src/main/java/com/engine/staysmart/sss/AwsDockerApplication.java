faaf71f2f2ebacdb8b22c9a9a91d01a720032105
sallubhai <abidhsk123@gmail.com>
2021-01-30 17:37:38 +0530

    checking initinal commit

---
+++ aws-docker/src/main/java/com/engine/staysmart/sss/AwsDockerApplication.java
@@ -0,0 +1,13 @@
+package com.engine.staysmart.sss;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsDockerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwsDockerApplication.class, args);
+	}
+
+}
---
+++ aws-docker/src/main/java/com/engine/staysmart/sss/controller/Controller.java
@@ -0,0 +1,15 @@
+package com.engine.staysmart.sss.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class Controller {
+	@GetMapping("/hello")
+    public String hello() {
+		return "welcome to docker image";
+    	
+    }
+}
